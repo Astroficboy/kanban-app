9d9c393619cb951f8bc5000eee894f545efcdc81
Pranav Wankhedkar <emailforcoding2493@gmail.com>
2022-09-13 20:54:09 +0530

    adding changed files

--- website/views.py
+++ website/views.py
@@ -18,7 +18,6 @@ def home():
     tasks_by_lists = {}
     for t in tasks:
         l = t.task_list_id
-        print(t.task_list_id, "------------------")
         tasks = None
         # tasks = tasks_by_lists[l]
         if not l in tasks_by_lists.keys():
@@ -27,11 +26,7 @@ def home():
         else:
             tasks = tasks_by_lists[l]
         tasks.append(t)
-
-    print(tasks_by_lists)
     return(render_template('lists.html', task_list=task_list, user=user, tasks_by_lists=tasks_by_lists))
-
-
 
 
 @views.route('/create_list', methods=['GET', 'POST'])
@@ -43,6 +38,7 @@ def create_list():
         db.session.add(update_task_list)    
         db.session.commit()
     if request.method == 'POST':
+        flash('New list added.', category='success')
         return((redirect(url_for('views.home'))))
     return(render_template('create_list.html', user=user))
     
@@ -55,26 +51,30 @@ def create_task():
     list_id = request.args.get('list_id')
     status = request.form.get('status')
     __status__= Task_status.Not_defined
-
     if request.method == 'POST':
         for s in Task_status:
             if s.name==status:
                 __status__=status
-
+        if description is None:
+            description = None
         update_task = Task(task_list_id=list_id, user_id=current_user.id, title=tasks_title, description=description, status=__status__, end_date=datetime.today()+timedelta(2))
         if tasks_title is not None:
             db.session.add(update_task)    
             db.session.commit()
-
+            flash('New task added.', category='success')
         return((redirect(url_for('views.home'))))
-
     return(render_template('create_task.html', user=user, list_id=list_id))
 
 
 @views.route('/delete_list', methods=['POST', 'GET'])
 def delete_list():
     user = User.query.filter_by(id = current_user.id).first()
+    list_id = request.args.get('list_id')
+    Task_list.query.filter_by(id=list_id).delete()
+    Task.query.filter_by(id=list_id).delete()
+    db.session.commit()
     if request.method == 'POST':
+        flash('List deleted.', category='success')
         return((redirect(url_for('views.home'))))
     return(render_template('delete_list.html', user=user))
 
@@ -82,7 +82,11 @@ def delete_list():
 @views.route('/delete_task', methods=['POST', 'GET'])
 def delete_task():
     user = User.query.filter_by(id = current_user.id).first()
+    task_id = request.args.get('task_id')
+    Task.query.filter_by(id=task_id).delete()
+    db.session.commit()
     if request.method == 'POST':
+        flash('Task deleted.', category='success')
         return((redirect(url_for('views.home'))))
-    return(render_template('delete_list.html', user=user))
+    return(render_template('delete_task.html', user=user))
 
